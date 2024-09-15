fd52ef6b38a75218bf76533ea883c6b46fb3cf86
baralsamrat <sbaral30064@ucumberlands.edu>
2024-09-15 14:09:00 -0500

    quicksort with random partition

--- part1.py
+++ part1.py
@@ -1,5 +1,15 @@
 import random
 
 def randomized_quicksort(arr):
-    """Sorts the array using Randomized Quicksort."""
+    def quicksort(low, high):
+        if low < high:
+            pivot_index = random.randint(low, high)
+            pivot_new_index = partition(low, high, pivot_index)
+            quicksort(low, pivot_new_index - 1)
+            quicksort(pivot_new_index + 1, high)
+    
+    def partition(low, high, pivot_index):
+        return
+
+    quicksort(0, len(arr) - 1)
     return arr
